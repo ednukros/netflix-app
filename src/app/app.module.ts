ea01c33a2ecb73ca26d7448f2d07f04af4f70a02
ednukros <isanmenarguez@gmail.com>
2023-02-01 16:27:00 +0100

    First commit. Project structured

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './components/nav/nav.component';
+import { FilmsComponent } from './components/films/films.component';
+import { HeroineComponent } from './components/heroine/heroine.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HeroineComponent,
+    FilmsComponent
   ],
   imports: [
     BrowserModule,
